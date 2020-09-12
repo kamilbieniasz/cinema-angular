2d56557d0ab05356e91d96e0193e764251c9d1da
kamilbieniasz <kamilbieniasz98@gmail.com>
2020-09-12 13:02:02 +0200

    Special offers

--- src/app/components/special-offers/special-offers.component.ts
+++ src/app/components/special-offers/special-offers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-special-offers',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialOffersComponent implements OnInit {
 
-  constructor() { }
+  forSchool = false;
+  forCompanies = false;
+  cheapWednesdays = false;
+  horrorsNight = false;
+  classic = false;
+
+  constructor(private viewprotScroller: ViewportScroller) { }
 
   ngOnInit(): void {
   }
 
+  changeFlagStatus(num: number): void{
+    this.clearFlag();
+    switch(num){
+      case 0:
+        this.forSchool = true;
+        break;
+      case 1:
+        this.forCompanies = true;
+        break;
+      case 2:
+        this.cheapWednesdays = true;
+        break;
+      case 3:
+        this.horrorsNight = true;
+        break;
+      case 4:
+        this.classic = true;
+        break;
+      default:
+        console.log("incorrect value");
+        break;
+    }
+  }
+  clearFlag(): void{
+    this.forSchool = false;
+    this.forCompanies = false;
+    this.cheapWednesdays = false;
+    this.horrorsNight = false;
+    this.classic = false;
+  }
+
+  scorllToElement(elementId: string): void{
+    this.viewprotScroller.scrollToAnchor(elementId);
+  }
 }
+
